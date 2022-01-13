68f5fe192618bbde63d1b3886d1a623321786fc7
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-01-13 10:08:19 +0900

    Tried to patch hasNotEnoughEnergy warning

--- src/main/java/net/eve0415/mc/gregpatcher/patch/PatchAbstractRecipeLogic.kt
+++ src/main/java/net/eve0415/mc/gregpatcher/patch/PatchAbstractRecipeLogic.kt
@@ -1,7 +1,9 @@
 package net.eve0415.mc.gregpatcher.patch
 
+import net.eve0415.mc.gregpatcher.GregPatcher
 import net.eve0415.mc.gregpatcher.Patch
-import org.objectweb.asm.Opcodes.ACC_PUBLIC
+import org.objectweb.asm.Opcodes.*
+import org.objectweb.asm.tree.*
 
 // Original: gregtech/api/capability/impl/AbstractRecipeLogic.java
 
@@ -12,6 +14,46 @@ class PatchAbstractRecipeLogic(inputClass: ByteArray) : Patch(inputClass) {
 
         val previousRecipe = findField("previousRecipe")
         previousRecipe?.access = ACC_PUBLIC
+
+        // This didn't work out so well on gregicadditions, but I'll leave it just in case
+        var insertionPoint: AbstractInsnNode? = null
+        val updateRecipeProgress = findMethod("updateRecipeProgress")!!.instructions
+
+        run {
+            val it: ListIterator<AbstractInsnNode> = updateRecipeProgress.iterator()
+            while (it.hasNext()) {
+                val insnNode: AbstractInsnNode = it.next()
+                if (insnNode.opcode == IFGE) {
+                    insertionPoint = insnNode
+                    break
+                }
+            }
+        }
+
+        if (insertionPoint == null) {
+            GregPatcher.LOGGER.warn("Could not find target instructions to patch. Skipping.")
+            return false
+        }
+
+        val newInst = InsnList()
+        newInst.add(VarInsnNode(ALOAD, 0))
+        newInst.add(InsnNode(ICONST_0))
+        newInst.add(
+            FieldInsnNode(
+                PUTFIELD,
+                "gregtech/api/capability/impl/AbstractRecipeLogic",
+                "hasNotEnoughEnergy",
+                "Z"
+            )
+        )
+        newInst.add(
+            MethodInsnNode(
+                INVOKESTATIC, hookClass, "check",
+                "()V",
+                false
+            )
+        )
+        updateRecipeProgress.insert(insertionPoint, newInst)
 
         return true
     }
